a90c0aa18083fa29b089f9b9fd99644495a754e5
Pascal Jungblut <mail@pascalj.de>
2023-02-23 20:39:35 +0100

    Start integrating machine model

--- fpga/host.cpp
+++ fpga/host.cpp
@@ -1,108 +1,31 @@
 #include "host.h"
+#include "json.hpp"
 #include "spdlog/spdlog.h"
+#include <CL/opencl.hpp>
 #include <algorithm>
+#include <boost/graph/directed_graph.hpp>
+#include <boost/graph/graphml.hpp>
 #include <cstdio>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
-#define CL_HPP_TARGET_OPENCL_VERSION 200
-#include <CL/opencl.hpp>
-#include <boost/graph/directed_graph.hpp>
-#include <boost/graph/graphml.hpp>
 
-using std::filesystem::path;
 
-using spdlog::info;
-
-void execute_dag(cl::Context &context, cl::Device &device) {
-  cl_int err;
-  aligned_vector<cl::Event> ooo_events;
-  aligned_vector<cl::Event> kernel_wait_events;
-  cl::CommandQueue queue(
-      context, device,
-      CL_QUEUE_PROFILING_ENABLE | CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE, &err);
-  const int matrix_scale_factor = 2;
-}
-
-void get_first_device(cl::Context &context, cl::Platform &platform,
-                      cl::Device &device) {
-  std::vector<cl::Platform> platforms;
-  cl::Platform::get(&platforms);
-  info("Found {} platforms:", platforms.size());
-  for (auto &platform : platforms) {
-    info("\t{}", platform.getInfo<CL_PLATFORM_NAME>());
-  }
-  assert(platforms.size() == 1);
-
-  // cheap to copy
-  platform = platforms[0];
-  info("Selected {}", platform.getInfo<CL_PLATFORM_NAME>());
-
-  std::vector<cl::Device> devices;
-  platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
-  info("Found {} devices:", devices.size());
-  for (auto &device : devices) {
-    info("\t{}", device.getInfo<CL_DEVICE_NAME>());
-  }
-  assert(platforms.size() >= 1);
-
-  device = devices[0];
-  info("Selected {}", device.getInfo<CL_DEVICE_NAME>());
-
-  cl::Context device_ctx(device);
-  context = device_ctx;
-}
-
-void read_binaries(const path directory, cl::Program::Binaries out,
-                   const std::string &extension = ".xclbin") {
-  for (auto entry : std::filesystem::directory_iterator{directory}) {
-    if (entry.is_regular_file() && entry.path().extension() == extension) {
-      std::ifstream is(entry.path(), std::ios::binary);
-      std::istream_iterator<unsigned char> start(is), end;
-      std::vector<unsigned char> binary(start, end);
-      out.push_back(std::move(binary));
-      info("Read {} bytes from {}", out.back().size(),
-           entry.path().filename().c_str());
-    }
-  }
-  info("Read {} binaries from {}", out.size(), directory.c_str());
-}
-
-std::string vec2str(std::vector<int>) { return ""; }
-auto str2vec(std::string const &str) -> std::vector<int> {
-  auto number = 0;
-  auto out = str;
-  auto vec = std::vector<int>{};
-  std::transform(str.cbegin(), str.cend(), out.begin(),
-                 [](char ch) { return (ch == ',') ? ' ' : ch; });
-  auto strs = std::stringstream{out};
-  while (strs >> number) {
-    vec.push_back(number);
-  }
-  return vec;
-}
-
-void read_graph(const path &graph_path, Graph &graph,
-                boost::dynamic_properties &properties) {
-  std::ifstream is(graph_path, std::ios::binary);
-  properties.property("label", boost::get(&Task::label, graph));
-  properties.property(
-      "comm",
-      TranslateStringPMap{boost::get(&Dependency::cost, graph), vec2str, str2vec});
-  properties.property(
-      "cost",
-      TranslateStringPMap{boost::get(&Task::cost, graph), vec2str, str2vec});
-  boost::read_graphml(is, graph, properties);
-  info("Imported {} with {} vertices and {} edges",
-       graph_path.filename().c_str(), boost::num_vertices(graph), boost::num_edges(graph));
-}
-
+/**
+ * @brief Execute task graphs using OpenCL
+ *
+ * Parse a task graph from GraphML and a schedule from a csv file and
+ * execute the schedules' task using a set of provided configurations.
+ *
+ */
 int main(int argc, char **argv) {
-  if (argc != 3) {
-    std::cout << "Usage: " << argv[0] << " <Graph.xml> <configuration.xclbin>"
-              << std::endl;
+  using std::filesystem::path;
+  using spdlog::info;
+
+  if (argc != 4) {
+    usage(argv[0]);
     return EXIT_FAILURE;
   }
 
@@ -110,16 +33,48 @@ int main(int argc, char **argv) {
   cl::Platform platform;
   cl::Device device;
 
-  std::filesystem::path graph_path(argv[1]);
-  std::filesystem::path xlbin_path(argv[2]);
+  cl_int err;
+  get_first_device(context, platform, device);
+
+  // Using an out-of-order queue, so we can have true parallelism
+  cl::CommandQueue queue(
+      context, device,
+      CL_QUEUE_PROFILING_ENABLE | CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE, &err);
+
+  path graph_path(argv[1]);
+  path xlbin_path(argv[2]);
+  path schedule_path(argv[3]);
+
   cl::Program::Binaries bins;
   read_binaries(xlbin_path, bins);
+  cl::Program program(context, {device}, bins, nullptr, &err);
 
   Graph graph;
   boost::dynamic_properties properties(boost::ignore_other_properties);
   read_graph(graph_path, graph, properties);
+  assert(num_vertices(graph) > 0);
 
+  Schedule schedule;
+  read_schedule_from_csv(schedule_path, schedule);
+  assert(schedule.size() == num_vertices(graph));
+}
+
+cl_int get_kernel_cost(const ScheduledTask& task) {
+  // TODO: project this to actual hardware numbers
+  return task.cost[task.PE];
+}
+
+void execute_dag_with_schedule(cl::Context &context, cl::Program &program,
+                               const Graph &graph, const Schedule &schedule) {
   cl_int err;
-  get_first_device(context, platform, device);
-  cl::Program program(context, {device}, bins, nullptr, &err);
+  aligned_vector<cl::Event> events;
+
+  // Mutable copy
+  Schedule sorted_schedule = schedule;
+  std::sort(sorted_schedule.begin(), sorted_schedule.end(),
+            [](const auto &lhs, const auto &rhs) { return lhs.t_s < rhs.t_s; });
+
+  for (auto &task : sorted_schedule) {
+    // enqueue...
+  }
 }
--- fpga/host.h
+++ fpga/host.h
@@ -1,28 +1,45 @@
 #pragma once
 
+#include "boost/graph/graphml.hpp"
+#include "boost/property_map/dynamic_property_map.hpp"
+#define CL_HPP_TARGET_OPENCL_VERSION 200
+#include "spdlog/spdlog.h"
+#include <CL/opencl.hpp>
 #include <boost/align/aligned_allocator.hpp>
 #include <boost/graph/directed_graph.hpp>
 #include <boost/property_map/property_map.hpp>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
 #include <string>
 #include <vector>
-#include <iostream>
-
-// Use a page-aligned vector
-template <typename T>
-using aligned_vector =
-    std::vector<T, boost::alignment::aligned_allocator<T, 4096>>;
 
 struct Task {
-  std::string label = "";
+  uint32_t id;
+  std::string label;
   std::vector<int> cost;
+};
+
+struct ScheduledTask : Task {
+  uint32_t t_s;
+  uint32_t PE;
 };
 
 struct Dependency {
   std::vector<int> cost;
 };
 
+// Use a page-aligned vector
+template <typename T>
+using aligned_vector =
+    std::vector<T, boost::alignment::aligned_allocator<T, 4096>>;
+
+// A schedule is the set of ScheduledTasks, since they contain t_s and the PE
+using Schedule = std::vector<ScheduledTask>;
+
 /* using Graph = boost::directed_graph<Task, Dependency>; */
-using Graph = boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, Task, Dependency>;
+using Graph = boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS,
+                                    Task, Dependency>;
 
 // https://stackoverflow.com/questions/73309661/how-to-interpret-complex-strings-as-graph-properties-when-reading-a-graphml-file
 template <typename PMap, typename ToString, typename FromString>
@@ -53,3 +70,128 @@ private:
   ToString to_string;
   FromString from_string;
 };
+
+
+inline void get_first_device(cl::Context &context, cl::Platform &platform,
+                      cl::Device &device) {
+  std::vector<cl::Platform> platforms;
+  cl::Platform::get(&platforms);
+  spdlog::info("Found {} platforms:", platforms.size());
+  for (auto &platform : platforms) {
+    spdlog::info("\t{}", platform.getInfo<CL_PLATFORM_NAME>());
+  }
+  assert(platforms.size() == 1);
+
+  // cheap to copy
+  platform = platforms[0];
+  spdlog::info("Selected {}", platform.getInfo<CL_PLATFORM_NAME>());
+
+  std::vector<cl::Device> devices;
+  platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
+  spdlog::info("Found {} devices:", devices.size());
+  for (auto &device : devices) {
+    spdlog::info("\t{}", device.getInfo<CL_DEVICE_NAME>());
+  }
+  assert(platforms.size() >= 1);
+
+  device = devices[0];
+  spdlog::info("Selected {}", device.getInfo<CL_DEVICE_NAME>());
+
+  cl::Context device_ctx(device);
+  context = device_ctx;
+}
+
+inline std::string vec2str(std::vector<int>);
+inline auto str2vec(std::string const &str) -> std::vector<int>;
+inline void read_graph(const std::filesystem::path &graph_path, Graph &graph,
+                boost::dynamic_properties &properties) {
+  std::ifstream is(graph_path, std::ios::binary);
+  properties.property("label", boost::get(&Task::label, graph));
+  properties.property("comm",
+                      TranslateStringPMap{boost::get(&Dependency::cost, graph),
+                                          vec2str, str2vec});
+  properties.property(
+      "cost",
+      TranslateStringPMap{boost::get(&Task::cost, graph), vec2str, str2vec});
+  boost::read_graphml(is, graph, properties);
+  spdlog::info("Imported {} with {} vertices and {} edges",
+       graph_path.filename().c_str(), boost::num_vertices(graph),
+       boost::num_edges(graph));
+}
+
+inline void read_schedule_from_csv(const std::filesystem::path &schedule_path, Schedule &schedule) {
+  std::ifstream is(schedule_path, std::ios::binary);
+  std::vector<std::string> result;
+  std::string line;
+
+  while (std::getline(is, line)) {
+    std::stringstream line_stream(line);
+    std::string tok;
+    ScheduledTask task;
+    std::getline(line_stream, tok, ',');
+    task.id = std::stoi(tok);
+    std::getline(line_stream, tok, ',');
+    task.t_s = std::stoi(tok);
+    std::getline(line_stream, tok, ',');
+    task.PE = std::stoi(tok);
+
+    schedule.push_back(std::move(task));
+  }
+}
+
+
+/**
+ * @brief Execute the DAG found in graph on device using schedule
+ *
+ * @param context 
+ * @param device 
+ * @param graph 
+ * @param schedule 
+ */
+void execute_dag_with_schedule(cl::Context &context, cl::Device &device, const Graph &graph,
+                 const Schedule &schedule);
+
+inline void read_binaries(const std::filesystem::path directory,
+                          cl::Program::Binaries out,
+                          const std::string &extension = ".xclbin") {
+  for (auto entry : std::filesystem::directory_iterator{directory}) {
+    if (entry.is_regular_file() && entry.path().extension() == extension) {
+      std::ifstream is(entry.path(), std::ios::binary);
+      std::istream_iterator<unsigned char> start(is), end;
+      std::vector<unsigned char> binary(start, end);
+      out.push_back(std::move(binary));
+      spdlog::info("Read {} bytes from {}", out.back().size(),
+                   entry.path().filename().c_str());
+    }
+  }
+  spdlog::info("Read {} binaries from {}", out.size(), directory.c_str());
+}
+
+inline std::string vec2str(std::vector<int>) { return ""; }
+inline auto str2vec(std::string const &str) -> std::vector<int> {
+  auto number = 0;
+  auto out = str;
+  auto vec = std::vector<int>{};
+  std::transform(str.cbegin(), str.cend(), out.begin(),
+                 [](char ch) { return (ch == ',') ? ' ' : ch; });
+  auto strs = std::stringstream{out};
+  while (strs >> number) {
+    vec.push_back(number);
+  }
+  return vec;
+}
+
+inline void usage(const std::string &prg) {
+  std::cout << "Usage: " << prg
+            << " <graph.xml> <configurations/> <schedule.csv>" << std::endl
+            << std::endl
+            << "\t<graph.xml>: path to a Graphml representation of the task "
+               "graph to schedule"
+            << std::endl
+            << "\t<configurations/>: path to a directory containing at least "
+               "one .xclbin"
+            << std::endl
+            << "\t<schedule.csv>: path to a CSV file containing rows of "
+               "'<task_id>,<t_s>,<pe_index>'"
+            << std::endl;
+}
