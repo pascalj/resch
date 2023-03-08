ae4088c5927d9881dcd261f5d96b19442779ca86
Pascal Jungblut <pascal.jungblut@ifi.lmu.de>
2023-03-08 15:46:05 +0100

    Include REFT testing

--- test/scheduling/test_reft.py
+++ test/scheduling/test_reft.py
@@ -3,6 +3,7 @@ import unittest
 from test.context import resch
 import resch.scheduling.reft as reft
 import resch.graph.taskgraph as graph
+import resch.evaluation.generator as generator
 
 from test.fixtures import fixtures
 
@@ -11,3 +12,12 @@ class TestREFT(unittest.TestCase):
         M = fixtures.minimal_machine()
         G = fixtures.sample_graph()
         S = reft.REFT(M, G).schedule()
+
+        self.assertEqual(len(S.tasks), G.num_nodes())
+
+    def test_reft_random(self):
+        M = fixtures.minimal_machine()
+        G = graph.TaskGraph(generator.random(12))
+        S = reft.REFT(M, G).schedule()
+
+        self.assertEqual(len(S.tasks), G.num_nodes())
